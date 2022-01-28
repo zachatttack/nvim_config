cc03aa04edcf5d995e4e3cc3e259c330283fcfe3
Zach Thomas <zach.thomas@distek.com>
2022-01-28 10:37:48 -0600

    Add transparent.lua

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,6 @@
 vim.cmd [[
 try
-  colorscheme darkplus
+  colorscheme nordfox
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,6 +62,10 @@ return packer.startup(function(use)
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
+  use "EdenEast/nightfox.nvim"
+  use "folke/tokyonight.nvim"
+  use "xiyaowong/nvim-transparent"
+
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
