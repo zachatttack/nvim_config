f52141f65e231c0cd34dbc35aa1b47081bb42622
Zach Thomas <zach.thomas@distek.com>
2022-03-09 12:04:39 -0600

    Add symbols outline plugin

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -26,6 +26,7 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 keymap("n", "<F4>", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<F5>", ":SymbolsOutline<CR>", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
