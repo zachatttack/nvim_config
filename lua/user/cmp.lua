883c75387b0d76695f960242c8e26239913c589d
Zach Thomas <zach.thomas1.zt@gmail.com>
2022-04-29 13:09:57 -0500

    Update api for cmp

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -52,8 +52,8 @@ cmp.setup {
     end,
   },
   mapping = {
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-		["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), {'i','c'}),
+    ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), {'i','c'}),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
