48f7455883f11145880755017258a2d5dde08d5a
Zach Thomas <zach.thomas1.zt@gmail.com>
2022-06-05 21:49:46 -0500

    Add clangd settings

---
+++ lua/user/lsp/settings/clangd.lua
@@ -0,0 +1,10 @@
+local util = require 'lspconfig.util'
+
+return {
+  cmd = {
+    "clangd",
+    '--query-driver=/home/zach/.espressif/tools/xtensa-esp32-elf/esp-2021r2-patch3-8.4.0/xtensa-esp32-elf/bin/xtensa-esp32-elf-gcc',
+  },
+  filetypes = {"c", "cpp"},
+  single_file_support = "true"
+}
