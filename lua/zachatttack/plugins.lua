d3892ee4d7b5b587caa097d09469b64386197664
Zach Thomas <zach.thomas1.zt@gmail.com>
2022-08-23 09:06:24 -0500

    Add markdown preview

--- lua/zachatttack/plugins.lua
+++ lua/zachatttack/plugins.lua
@@ -137,6 +137,12 @@ return packer.startup(function(use)
 
   use "kylechui/nvim-surround"
 
+-- install without yarn or npm
+  use({
+      "iamcco/markdown-preview.nvim",
+      run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
